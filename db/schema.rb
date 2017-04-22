c7f9c7e587269c9d17089531dd55e2074203f055
Mateo Vidal <jvidalba@gmail.com>
2017-04-22 18:34:16 -0500

    Change db adapter

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20170421224109) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "categories", force: :cascade do |t|
     t.string   "en"
     t.string   "es"
